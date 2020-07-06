e98205d9177ab10222718118b17ae9efb51d7939
azer <azerprojects@gmail.com>
2020-07-06 07:41:01 +0700

    initialize

--- src/main/java/com/azer/meetingmanager/App.java
+++ src/main/java/com/azer/meetingmanager/App.java
@@ -1,13 +1,23 @@
 package com.azer.meetingmanager;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        primaryStage.setTitle("Meetings Manager");
+
+
+        primaryStage.setScene(new MainScene(new BorderPane(), 1440, 1024));
+
+        primaryStage.show();
     }
 }
---
+++ src/main/java/com/azer/meetingmanager/MainScene.java
@@ -0,0 +1,12 @@
+package com.azer.meetingmanager;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class MainScene extends Scene {
+
+    public MainScene(Parent root, double width, double height) {
+        super(root, width, height);
+    }
+    
+}
