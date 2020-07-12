1858adc0aae120e8720ccb84ed2d6fe72bd8f046
azer <azerprojects@gmail.com>
2020-07-12 08:31:39 +0700

    + meeting editor and pending request page

---
+++ src/main/java/com/azer/meetingmanager/ui/admin/MeetingEditorController.java
@@ -0,0 +1,71 @@
+package com.azer.meetingmanager.ui.admin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+public class MeetingEditorController implements Initializable {
+
+    @FXML
+    private StackPane photoEmptyPane;
+
+    @FXML
+    private ImageView photoImageView;
+
+    @FXML
+    private TextField titleTextField;
+
+    @FXML
+    private TextField dateTextField;
+
+    @FXML
+    private TextField monthTextField;
+
+    @FXML
+    private TextField yearTextField;
+
+    @FXML
+    private TextField hourTextField;
+
+    @FXML
+    private TextField minuteTextField;
+
+    @FXML
+    private TextField locationNameTextField;
+
+    @FXML
+    private TextField locationAddressTextFIeld;
+
+    @FXML
+    private TextField locationCapacityTextField;
+
+    @FXML
+    private TextArea briefDescriptionTextField;
+
+    @FXML
+    private TextArea detailDescriptionTextField;
+
+    @FXML
+    void onRestore(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSave(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/java/com/azer/meetingmanager/ui/admin/PendingRequestController.java
@@ -0,0 +1,31 @@
+package com.azer.meetingmanager.ui.admin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+
+public class PendingRequestController implements Initializable {
+
+    @FXML
+    private ScrollPane pendingContainer;
+
+    @FXML
+    void onAcceptAll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDenyAll(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+    }
+
+}
