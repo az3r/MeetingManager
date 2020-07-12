c9af5c9e76e20f2bf2a8c6ddcd4892d754b8754d
azer <azerprojects@gmail.com>
2020-07-12 09:11:44 +0700

    + UserItemController

---
+++ src/main/java/com/azer/meetingmanager/ui/components/UserItemController.java
@@ -0,0 +1,45 @@
+package com.azer.meetingmanager.ui.components;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class UserItemController implements Initializable {
+
+    @FXML
+    private Text userNameText;
+
+    @FXML
+    private Text userNameEmail;
+
+    @FXML
+    void onAcceptRequest(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onBlockUser(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onDenyRequest(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onUnBlockUser(MouseEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
