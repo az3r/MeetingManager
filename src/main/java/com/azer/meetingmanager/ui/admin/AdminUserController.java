62be821d4f458335c1df149db1753a2121c815d4
azer <azerprojects@gmail.com>
2020-07-12 09:24:28 +0700

    + AdminUser

---
+++ src/main/java/com/azer/meetingmanager/ui/admin/AdminUserController.java
@@ -0,0 +1,22 @@
+package com.azer.meetingmanager.ui.admin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+
+public class AdminUserController {
+
+    @FXML
+    private ScrollPane uesrItemContainer;
+
+    @FXML
+    void onSortByEmailAsc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSortByNameAsc(ActionEvent event) {
+
+    }
+
+}
